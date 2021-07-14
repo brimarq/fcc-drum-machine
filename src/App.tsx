d47801e1535b26bce79229be272053cab9319049
Brian Marquis <rbmarquis.dev@gmail.com>
2021-07-13 22:10:00 -0500

    convert App to FC

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import DrumMachine from './components/DrumMachine';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    // console.log("App render called");
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-        <main className="App-main">
-          <DrumMachine />
-        </main>
-        <footer className="App-footer"></footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main className="App-main">
+        <DrumMachine />
+      </main>
+      <footer className="App-footer"></footer>
+    </div>
+  );
 }
 
 export default App;
