2fb3da7a641262266f9a3bb11a6b0fb8c8e6a62b
Brian Marquis <rbmarquis.dev@gmail.com>
2021-07-18 23:10:17 -0500

    convert to FC

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,71 +2,69 @@ import React from 'react';
 import './Controls.css';
 
 interface ControlsProps {
-  isPwrOn: boolean;
   display: string;
   volume: number;
-  isBank1: boolean;
-  handleChange: (e: any) => void;
+  handleChange: (event: any) => void;
 }
 
-export default class Controls extends React.PureComponent<ControlsProps> {
-  render() {
-    // console.log("Controls render called");
-    return (
-      <div id="controls">
-        <p>FCC DrumMachine</p>
-        <div id="display">{this.props.display}</div>
+function Controls(props: ControlsProps) {
+  const { display, volume, handleChange } = props;
+  return (
+    <div id="controls">
+      <p>FCC DrumMachine</p>
+      <div id="display">{display}</div>
 
-        <div className="switch-container">
-          <span className="switch-title">Power</span>
-          <div className="switch">
-            <input
-              type="checkbox"
-              name="power"
-              id="switch-power"
-              className="switch-checkbox"
-              defaultChecked
-              onChange={this.props.handleChange}
-            />
-            <label className="switch-label" htmlFor="switch-power">
-              <span className="switch-inner"></span>
-              <span className="switch-switch"></span>
-            </label>
-          </div>
-        </div>
-
-        <div className="switch-container">
-          <span className="switch-title">Bank</span>
-          <div className="switch">
-            <input
-              type="checkbox"
-              name="bank"
-              id="switch-bank"
-              className="switch-checkbox"
-              defaultChecked
-              onChange={this.props.handleChange}
-            />
-            <label className="switch-label" htmlFor="switch-bank">
-              <span className="switch-inner"></span>
-              <span className="switch-switch"></span>
-            </label>
-          </div>
-        </div>
-
-        <div className="slider-container">
-          <span className="slider-title">Volume</span>
+      <div className="switch-container">
+        <span className="switch-title">Power</span>
+        <div className="switch">
           <input
-            type="range"
-            min="0"
-            max="1"
-            step=".01"
-            defaultValue={this.props.volume}
-            className="slider"
-            id="myRange"
-            onChange={this.props.handleChange}
+            type="checkbox"
+            name="power"
+            id="switch-power"
+            className="switch-checkbox"
+            defaultChecked
+            onChange={handleChange}
           />
+          <label className="switch-label" htmlFor="switch-power">
+            <span className="switch-inner"></span>
+            <span className="switch-switch"></span>
+          </label>
         </div>
       </div>
-    );
-  }
+
+      <div className="switch-container">
+        <span className="switch-title">Bank</span>
+        <div className="switch">
+          <input
+            type="checkbox"
+            name="bank"
+            id="switch-bank"
+            className="switch-checkbox"
+            defaultChecked
+            onChange={handleChange}
+          />
+          <label className="switch-label" htmlFor="switch-bank">
+            <span className="switch-inner"></span>
+            <span className="switch-switch"></span>
+          </label>
+        </div>
+      </div>
+
+      <div className="slider-container">
+        <span className="slider-title">Volume</span>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step=".01"
+          defaultValue={volume}
+          className="slider"
+          id="myRange"
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
 }
+
+export default Controls;
